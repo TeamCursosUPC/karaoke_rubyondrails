1eadf2b4db3518664d7101a7db2f343ebc548723
Alan Cornejo <alancornejo@icloud.com>
2016-09-20 21:58:10 -0500

    Agregar Ejemplo de Seeder

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Seeders Productos
+ Product.destroy_all
+ products = Product.create([
+    { nombre: 'Jhonie Walker Red Laber', tipo: 'Bebida',precio: '210.00', disponibilidad: '1' },
+    { nombre: 'Ron Cartavio', tipo: 'Bebida',precio: '95.00', disponibilidad: '1' }
+ ])
+ p "Created #{Product.count} Products"
