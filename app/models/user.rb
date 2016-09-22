fbf22c7d8f840b928ae36044e48da334ac775fd0
Alan Cornejo <alancornejo@icloud.com>
2016-09-21 20:47:32 -0500

    Mejorar Tablas Users, el cual habia sido mal realizado su merge

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,26 +1,10 @@
-# == Schema Information
-#
-# Table name: users
-#
-#  id               :integer          not null, primary key
-#  Nombre           :string
-#  Apellido_Paterno :string
-#  Apellido_Materno :string
-#  Correo           :string
-#  Password         :string
-#  DNI              :integer
-#  Telefono         :integer
-#  created_at       :datetime         not null
-#  updated_at       :datetime         not null
-#
-
 class User < ApplicationRecord
   #Validamos los datos del usuario que se agregue
   # mostramos el mensaje de error con message
   validates :Nombre , confirmation: true, presence: { message: " dato obligatorio"}
   validates :Apellido_Paterno , confirmation: true, presence: { message: " dato obligatorio"}
   validates :Apellido_Materno , confirmation: true, presence: { message: " dato obligatorio"}
-  validates :Telefono, confirmation: true, presence: { message: " dato obligatorio"}
+ validates :Telefono, confirmation: true, presence: { message: " dato obligatorio"}
  
   # Validamos expresiones regulares de correo
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
