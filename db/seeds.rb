ded91ce2f4200b97567199a431dc8e0f6f922690
Alan Cornejo <alancornejo@icloud.com>
2016-10-08 14:12:53 -0500

    Mejoras de Productos y de Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,15 +3,15 @@
 Local.destroy_all
 ActiveRecord::Base.connection.execute("DELETE FROM sqlite_sequence WHERE name = 'locals';")
 locals = Local.create([
-													{ nombre: 'SAN JOSE DEL REFUGIO', direccion: 'FORESTA PIRUL 123', telefono: '279652', mail: 'SANJOSEDELREFUGIO@karaoke.com' , descripcion: 'Descripcion 1', administrador: 'ABOITES'},
-													{ nombre: 'CENTRO', direccion: 'PROLONGACION MADERO 977-A', telefono: '466663', mail: 'CENTRO@karaoke.com' , descripcion: 'Descripcion 2', administrador: 'ABONCE'},
-													{ nombre: 'NOCHE BUENA', direccion: 'ALLENDE 305', telefono: '114121', mail: 'NOCHEBUENA@karaoke.com' , descripcion: 'Descripcion 3', administrador: 'ABOYTES'},
-													{ nombre: 'OASIS', direccion: 'I. ALDAMA No.  203', telefono: '114111', mail: 'OASIS@karaoke.com' , descripcion: 'Descripcion 4', administrador: 'ACOSTA'},
-													{ nombre: 'PENA TAURINA', direccion: 'LIBERTAD 58', telefono: '643571', mail: 'PENATAURINA@karaoke.com' , descripcion: 'Descripcion 5', administrador: 'AGUILAR'},
-													{ nombre: 'CENTRO HISTORICO', direccion: 'INSURGENTES SUR 1106', telefono: '548881', mail: 'CENTROHISTORICO@karaoke.com' , descripcion: 'Descripcion 6', administrador: 'AGUILERA'},
-													{ nombre: 'DEL VALLE', direccion: 'KM.16.5 BLV. CAP. CARLOS CAMACHO', telefono: '112222', mail: 'DELVALLE@karaoke.com' , descripcion: 'Descripcion 7', administrador: 'AGUIRRE'},
-													{ nombre: 'SANTA MARIA DEL GRANJENO', direccion: 'ZACATECAS  3', telefono: '114181', mail: 'SANTAMARIADELGRANJENO@karaoke.com' , descripcion: 'Descripcion 8', administrador: 'ALANIS'},
-													{ nombre: 'MARAVATIO', direccion: 'MANUEL CAPETILLO S/N', telefono: '419193', mail: 'MARAVATIO@karaoke.com' , descripcion: 'Descripcion 9', administrador: 'ALATORRE'}
+		{ nombre: 'SAN JOSE DEL REFUGIO', direccion: 'FORESTA PIRUL 123', telefono: '279652', mail: 'SANJOSEDELREFUGIO@karaoke.com' , descripcion: 'Descripcion 1', administrador: 'ABOITES'},
+		{ nombre: 'CENTRO', direccion: 'PROLONGACION MADERO 977-A', telefono: '466663', mail: 'CENTRO@karaoke.com' , descripcion: 'Descripcion 2', administrador: 'ABONCE'},
+		{ nombre: 'NOCHE BUENA', direccion: 'ALLENDE 305', telefono: '114121', mail: 'NOCHEBUENA@karaoke.com' , descripcion: 'Descripcion 3', administrador: 'ABOYTES'},
+		{ nombre: 'OASIS', direccion: 'I. ALDAMA No.  203', telefono: '114111', mail: 'OASIS@karaoke.com' , descripcion: 'Descripcion 4', administrador: 'ACOSTA'},
+		{ nombre: 'PENA TAURINA', direccion: 'LIBERTAD 58', telefono: '643571', mail: 'PENATAURINA@karaoke.com' , descripcion: 'Descripcion 5', administrador: 'AGUILAR'},
+		{ nombre: 'CENTRO HISTORICO', direccion: 'INSURGENTES SUR 1106', telefono: '548881', mail: 'CENTROHISTORICO@karaoke.com' , descripcion: 'Descripcion 6', administrador: 'AGUILERA'},
+		{ nombre: 'DEL VALLE', direccion: 'KM.16.5 BLV. CAP. CARLOS CAMACHO', telefono: '112222', mail: 'DELVALLE@karaoke.com' , descripcion: 'Descripcion 7', administrador: 'AGUIRRE'},
+		{ nombre: 'SANTA MARIA DEL GRANJENO', direccion: 'ZACATECAS  3', telefono: '114181', mail: 'SANTAMARIADELGRANJENO@karaoke.com' , descripcion: 'Descripcion 8', administrador: 'ALANIS'},
+		{ nombre: 'MARAVATIO', direccion: 'MANUEL CAPETILLO S/N', telefono: '419193', mail: 'MARAVATIO@karaoke.com' , descripcion: 'Descripcion 9', administrador: 'ALATORRE'}
 											])
 p "Created #{Local.count} Locals"
 
@@ -84,6 +84,7 @@ p "Created #{Song.count} Songs"
 	{ nombre: 'Tierra Blanca', capacidad: '8', descripcion:  'Descipcion sala 9', local_id:  '3' }
  ])
  p "Created #{Room.count} Rooms"
+
 
 #Seeders Reservas
 Reserf.destroy_all
