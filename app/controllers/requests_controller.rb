00b3053b6e9e5645fd2a116a6c333a5234fad862
Alan Cornejo <alancornejo@icloud.com>
2016-10-09 12:37:57 -0500

    Crear Pedido

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -28,7 +28,8 @@ class RequestsController < KaraokeController
 
     respond_to do |format|
       if @request.save
-        format.html { redirect_to @request, notice: 'Request was successfully created.' }
+
+        format.html { redirect_to @request, notice: 'El Pedido ha sido creado.' }
         format.json { render :show, status: :created, location: @request }
       else
         format.html { render :new }
@@ -42,7 +43,7 @@ class RequestsController < KaraokeController
   def update
     respond_to do |format|
       if @request.update(request_params)
-        format.html { redirect_to @request, notice: 'Request was successfully updated.' }
+        format.html { redirect_to @request, notice: 'El pedido ha sido actualizado.' }
         format.json { render :show, status: :ok, location: @request }
       else
         format.html { render :edit }
@@ -56,7 +57,7 @@ class RequestsController < KaraokeController
   def destroy
     @request.destroy
     respond_to do |format|
-      format.html { redirect_to requests_url, notice: 'Request was successfully destroyed.' }
+      format.html { redirect_to requests_url, notice: 'El Pedido ha sido eliminado.' }
       format.json { head :no_content }
     end
   end
